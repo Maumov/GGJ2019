6e8c3b7a9743ff4badfe7363d591ca3a7a6e9f00
Antonio <antonyurieta@hotmail.com>
2019-05-08 22:56:35 -0500

    lantern

--- GGJ2019/Assets/Scripts/Rotate.cs
+++ GGJ2019/Assets/Scripts/Rotate.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    [Range(0f, 1f)]
+    public float movementSpeed;
     public float speed = 100;
+    public bool isItem = false;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.World);
+        if (isItem)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Mathf.Sin(Time.time) * Time.deltaTime * movementSpeed, transform.position.z);
+        }
     }
 }
